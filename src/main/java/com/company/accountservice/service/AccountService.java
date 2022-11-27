652058791152195616a1d586e83de40a2f0b45f7
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-27 23:22:28 +0400

    AccountService

---
+++ src/main/java/com/company/accountservice/service/AccountService.java
@@ -0,0 +1,26 @@
+package com.company.accountservice.service;
+
+import com.company.accountservice.entity.Account;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+    public Account get(String id) {
+
+    }
+    public Account save(Account account) {
+
+    }
+
+    public Account update(Account account) {
+
+    }
+
+    public void delete(String id) {
+
+    }
+
+
+
+}
