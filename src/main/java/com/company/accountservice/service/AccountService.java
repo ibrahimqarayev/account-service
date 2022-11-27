f065a060664cb857fe9afee73ac9173882c0702e
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-27 23:30:58 +0400

    AccountService

--- src/main/java/com/company/accountservice/service/AccountService.java
+++ src/main/java/com/company/accountservice/service/AccountService.java
@@ -5,16 +5,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
+    //TEST
 
     public Account get(String id) {
-
+        return new Account("test-id");
     }
     public Account save(Account account) {
-
+        return new Account("test-id");
     }
 
     public Account update(Account account) {
-
+        return new Account("test-id");
     }
 
     public void delete(String id) {
