6ac39a291e08ec2b48aad7294e4049113df476a1
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-02 22:39:51 +0400

    Account

--- src/main/java/com/company/accountservice/entity/Account.java
+++ src/main/java/com/company/accountservice/entity/Account.java
@@ -66,4 +66,14 @@ public class Account {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", passwd='" + passwd + '\'' +
+                '}';
+    }
 }
