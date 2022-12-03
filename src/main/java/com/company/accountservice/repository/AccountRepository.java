d2e8855e7c186a0e2aefd69e26f6839949fe8a5e
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-03 22:43:13 +0400

    AccountRepository

--- src/main/java/com/company/accountservice/repository/AccountRepository.java
+++ src/main/java/com/company/accountservice/repository/AccountRepository.java
@@ -1,7 +1,9 @@
 package com.company.accountservice.repository;
 
+import com.company.accountservice.entity.Account;
+import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository {
+public interface AccountRepository extends CassandraRepository<Account,String> {
 }
