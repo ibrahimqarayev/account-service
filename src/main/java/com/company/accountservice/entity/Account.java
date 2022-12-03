7a1e4b3404b34ea06a939536f7ad17f0cb9f0e06
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-03 22:35:30 +0400

    Account

--- src/main/java/com/company/accountservice/entity/Account.java
+++ src/main/java/com/company/accountservice/entity/Account.java
@@ -5,6 +5,7 @@ import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -18,6 +19,10 @@ public class Account implements Serializable {
     private String email;
     @Column(value = "pwd")
     private String password;
+    @Column(value = "created_at")
+    private Date createdAt;
+    @Column(value = "is_active")
+    private Boolean active;
 
     public Account() {
     }
