e3bbf26ec42eca163ae3a4b5ae0a97ebb93ab9f1
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-03 22:42:10 +0400

    AccountService

--- src/main/java/com/company/accountservice/service/AccountService.java
+++ src/main/java/com/company/accountservice/service/AccountService.java
@@ -1,15 +1,23 @@
 package com.company.accountservice.service;
 
 import com.company.accountservice.entity.Account;
+import com.company.accountservice.repository.AccountRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
     //TEST
 
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
     public Account get(String id) {
         return new Account("test-id");
     }
+
     public Account save(Account account) {
         return new Account("test-id");
     }
@@ -21,7 +29,6 @@ public class AccountService {
     public void delete(String id) {
 
     }
-
 
 
 }
