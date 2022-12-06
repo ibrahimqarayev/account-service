be85c940b23f6e2ce4e714b784c3e3e1a499e169
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-06 23:41:35 +0400

    AccountService

--- src/main/java/com/company/accountservice/service/AccountService.java
+++ src/main/java/com/company/accountservice/service/AccountService.java
@@ -2,19 +2,16 @@ package com.company.accountservice.service;
 
 import com.company.accountservice.entity.Account;
 import com.company.accountservice.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
-
+@RequiredArgsConstructor
 @Service
 public class AccountService {
 
     //TEMPLATE SERVICE
 
     private final AccountRepository accountRepository;
-
-    public AccountService(AccountRepository accountRepository) {
-        this.accountRepository = accountRepository;
-    }
 
     public Account get(String id) {
         return accountRepository.findById(id).orElseThrow(() -> new IllegalStateException());
